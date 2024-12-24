178c2d244c24234c6f9b9a6f42ed709c80011dac
Sirmisa <misael.pinedo@hotmail.com>
2024-12-23 23:32:19 -0600

    asdsad

---
+++ practica.js
@@ -0,0 +1,8 @@
+// Eliminar todos los espacios en la cadena
+function eliminarEspacios(cadena) {
+    return cadena.replace(/\s+/g, '');
+}
+
+let texto = "Hola mundo con espacios";
+let resultado = eliminarEspacios(texto);
+console.log(resultado); // "Holamundoconespacios"
