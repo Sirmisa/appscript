5671410da458e4aebed0f1d8a71849d53cfc1bd6
Sirmisa <misael.pinedo@hotmail.com>
2024-12-24 01:32:38 -0600

    yrtu

--- practica.js
+++ practica.js
@@ -6,5 +6,21 @@ function eliminarEspacios(cadena) {
 let texto = "Hola mundo con espacios";
 let resultado = eliminarEspacios(texto);
 console.log(resultado); // "Holamundoconespacios"
-gygyugu
-sfsd
+
+// Eliminar todos los espacios en la cadena
+function eliminarEspacios(cadena) {
+    return cadena.replace(/\s+/g, '');
+}
+
+// Eliminar elementos duplicados en un array
+function eliminarDuplicados(arr) {
+    return [...new Set(arr)];
+}
+
+let texto = "Hola mundo con espacios";
+let resultado = eliminarEspacios(texto);
+console.log(resultado); // "Holamundoconespacios"
+
+let array = [1, 2, 2, 3, 4, 4, 5];
+let arraySinDuplicados = eliminarDuplicados(array);
+console.log(arraySinDuplicados); // [1, 2, 3, 4, 5]
